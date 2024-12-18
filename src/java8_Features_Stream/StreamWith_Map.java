707862d5b621806327bd66afa81425f5664cde17
Hemapalavalli <hemapalavalli8@gmail.com>
2024-12-18 17:08:52 +0530

    Stream

--- src/java8_Features_Stream/StreamWith_Map.java
+++ src/java8_Features_Stream/StreamWith_Map.java
@@ -1,25 +1,19 @@
 package java8_Features_Stream;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class StreamWith_Map {
 
 	public static void main(String[] args) {
-		
-		List<String> l=Arrays.asList("Java","DotNet","SQL","MongoDB","ReactJs");
-		
+
+		List<String> l = Arrays.asList("Java", "DotNet", "SQL", "MongoDB", "ReactJs");
+
 //		System.out.println(l.stream().map(lm->lm.toUpperCase()).toList());
-		
-		
-		System.out.println(l.stream().map(ml -> ml.toLowerCase()).
-				filter(m1->m1.startsWith("D"))
-				.map(m1 -> {
-					m1 = m1.toUpperCase();
-					m1=m1+" Full Stack";
-					return m1;
-				}).toList());
 
+		System.out.println(l.stream().map(m -> m.toUpperCase()).filter(m->m.startsWith("S")).map(m1->m1.toLowerCase()).map(m1 ->"My "+ m1).toList());
+		
+		System.out.println(l.stream().map(ml -> ml.toLowerCase()).filter(m->m.startsWith("j")).map(m1->m1.toUpperCase()).map(m -> m + " Full Stack").toList());
 	}
-
 }
